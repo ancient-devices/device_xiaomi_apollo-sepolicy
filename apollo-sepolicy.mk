fd7f07bf8d0925e28971e9205427e8fce105f1d4
goodmeow <harunbam3@gmail.com>
2021-10-30 11:17:15 +0200

    apollo: remove adbd definition

--- apollo-sepolicy.mk
+++ apollo-sepolicy.mk
@@ -18,7 +18,6 @@ BOARD_SEPOLICY_DIRS += \
 
 # Xiaomi Sepolicy
 BOARD_SEPOLICY_DIRS += \
-    device/xiaomi/apollo-sepolicy/vendor/xiaomi/adbd \
     device/xiaomi/apollo-sepolicy/vendor/xiaomi/audio \
     device/xiaomi/apollo-sepolicy/vendor/xiaomi/battery \
     device/xiaomi/apollo-sepolicy/vendor/xiaomi/block \
